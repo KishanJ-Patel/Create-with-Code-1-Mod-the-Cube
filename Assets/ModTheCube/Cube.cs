12aa9989521b2ac3affb902741a402271edeb89b
Kishan J Patel <kishan.patel2002@outlook.com>
2023-08-11 23:27:23 -0600

    Change the scale of the cube over time

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -9,13 +9,16 @@ public class Cube : MonoBehaviour
     [SerializeField] private float posZRange = 4f;
     [SerializeField] private float speedZAxis = 1f;
 
+    [SerializeField] private Vector3 minScaleLimit = Vector3.one / 2f;
+    [SerializeField] private Vector3 maxScaleLimit = Vector3.one * 1.5f;
+    [SerializeField] private float scaleRate = 0.5f;
+
     private bool moveForward = false;
+    private bool scaleUp = false;
+    private Vector3 newScale;
 
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
         Material material = Renderer.material;
         
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
@@ -25,11 +28,56 @@ public class Cube : MonoBehaviour
     {
         transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
 
-        if (!moveForward ) transform.Translate(Vector3.back * speedZAxis * Time.deltaTime, Space.World);
+        MoveAlongZAxis();
+
+        ChangeScale();
+    }
+
+    private void MoveAlongZAxis()
+    {
+        // Move the cube forward or backward
+        if (!moveForward) transform.Translate(Vector3.back * speedZAxis * Time.deltaTime, Space.World);
         else transform.Translate(Vector3.forward * speedZAxis * Time.deltaTime, Space.World);
 
+        // Check if the cube reached range limit
         if (transform.position.z <= -posZRange) moveForward = true;
         else if (transform.position.z >= posZRange) moveForward = false;
+    }
 
+    private void ChangeScale()
+    {
+        // Scale up or down the cube
+        if (!scaleUp)
+        {
+            newScale = new Vector3(
+                transform.localScale.x - (scaleRate * Time.deltaTime),
+                transform.localScale.y - (scaleRate * Time.deltaTime),
+                transform.localScale.z - (scaleRate * Time.deltaTime)
+                );
+            transform.localScale = newScale;
+        }
+        else
+        {
+            newScale = new Vector3(
+                transform.localScale.x + (scaleRate * Time.deltaTime),
+                transform.localScale.y + (scaleRate * Time.deltaTime),
+                transform.localScale.z + (scaleRate * Time.deltaTime)
+                );
+            transform.localScale = newScale;
+        }
+
+        // Check if the cube reached scale limit
+        if (transform.localScale.x <= minScaleLimit.x
+                || transform.localScale.y <= minScaleLimit.y
+                || transform.localScale.z <= minScaleLimit.z)
+        {
+            scaleUp = true;
+        }
+        else if (transform.localScale.x >= maxScaleLimit.x
+                || transform.localScale.y >= maxScaleLimit.y
+                || transform.localScale.z >= maxScaleLimit.z)
+        {
+            scaleUp = false;
+        }
     }
 }
